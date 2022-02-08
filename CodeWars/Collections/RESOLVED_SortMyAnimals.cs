e06d4df87aa84a11caf168814b991f76f04a9b8c
eugene <bykov.ey@gmail.com>
2022-02-08 19:51:38 +1000

    Таков путь

---
+++ CodeWars/Collections/RESOLVED_SortMyAnimals.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+//https://www.codewars.com/kata/58ff1c8b13b001a5a50005b4/train/csharp
+namespace CodeWars.Collections
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int NumberOfLegs { get; set; }
+    }
+
+    public class AnimalSorter
+    {
+        public List<Animal>? Sort(List<Animal> input)
+        {
+            return input?.OrderBy(i => i.Name).OrderBy(i => i.NumberOfLegs).ToList();
+        }
+    }
+
+    [TestFixture]
+    public class AnimalSorterTests
+    {
+        [Test]
+        public void SortTest()
+        {
+            var animals = new List<Animal>
+      {
+        new Animal {Name = "Cat", NumberOfLegs = 4},
+        new Animal {Name = "Snake", NumberOfLegs = 0},
+        new Animal {Name = "Dog", NumberOfLegs = 4},
+        new Animal {Name = "Pig", NumberOfLegs = 4},
+        new Animal {Name = "Human", NumberOfLegs = 2},
+        new Animal {Name = "Bird", NumberOfLegs = 2}
+      };
+            var output = new AnimalSorter().Sort(animals);
+            Assert.AreEqual(output[0].Name, "Snake");
+            Assert.AreEqual(output[3].Name, "Cat");
+        }
+    }
+}
---
+++ CodeWars/REFACTORING/RESOLVED_RefactorOutASwitchStatementUsingPolymorphism.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace CodeWars.REFACTORING
+{
+    internal class RESOLVED_RefactorOutASwitchStatementUsingPolymorphism
+    {
+        private readonly Status _status;
+
+        public RESOLVED_RefactorOutASwitchStatementUsingPolymorphism()
+            : this(new DefaultStatus())
+        {
+        }
+
+        public RESOLVED_RefactorOutASwitchStatementUsingPolymorphism(Status status)
+        {
+            _status = status;
+        }
+
+        public string GetStatusDescription()
+        {
+            return _status.GetStatusDescription();
+        }
+    }
+
+    public abstract class Status
+    {
+        public abstract string GetStatusDescription();
+    }
+
+    class DefaultStatus : Status
+    {
+        public override string GetStatusDescription()
+        {
+            return "I have never been set";
+        }
+    }
+
+    class NewStatus : Status
+    {
+        public override string GetStatusDescription()
+        {
+            return "I am new!";
+        }
+    }
+
+    class ActiveStatus : Status
+    {
+        public override string GetStatusDescription()
+        {
+            return "I am active";
+        }
+    }
+
+    class DeactivatedStatus : Status
+    {
+        public override string GetStatusDescription()
+        {
+            return "I have been deactivated";
+        }
+    }
+
+    [TestFixture]
+    public class ExampleTestCases
+    {
+        [Test]
+        public void StatusCorrect()
+        {
+            Assert.AreEqual("I have never been set", new RESOLVED_RefactorOutASwitchStatementUsingPolymorphism().GetStatusDescription());
+            Assert.AreEqual("I am new!", new RESOLVED_RefactorOutASwitchStatementUsingPolymorphism(new NewStatus()).GetStatusDescription());
+            Assert.AreEqual("I am active", new RESOLVED_RefactorOutASwitchStatementUsingPolymorphism(new ActiveStatus()).GetStatusDescription());
+            Assert.AreEqual("I have been deactivated", new RESOLVED_RefactorOutASwitchStatementUsingPolymorphism(new DeactivatedStatus()).GetStatusDescription());
+        }
+
+        [Test]
+        public void HasStatusField()
+        {
+            Type type = typeof(RESOLVED_RefactorOutASwitchStatementUsingPolymorphism);
+            FieldInfo field = type.GetField("_status", BindingFlags.NonPublic | BindingFlags.Instance);
+            Assert.IsNotNull(field);
+            Type fieldType = field.FieldType;
+            Assert.AreEqual(fieldType, typeof(Status));
+        }
+    }
+}
